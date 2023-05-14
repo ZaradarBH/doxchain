9b74fe355249331bf9d8fffbf3e975c9ae26e291
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-15 00:18:11 +0200

    fix initial issues with regexp, except params

--- utils/did_factory.go
+++ utils/did_factory.go
@@ -1,6 +1,9 @@
 package utils
 
 import (
+	"fmt"
+	"regexp"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	didTypes "github.com/be-heroes/doxchain/x/did/types"
 )
@@ -19,15 +22,14 @@ const (
 )
 
 var (
-	//TODO: Golang regexp does not support named captures, only numbered captures.
-	// REGEX_DID_METHOD_NAME = regexp.MustCompile(fmt.Sprintf("(?<%s>[a-zA-Z0-9_]+)", REGEX_DID_METHOD_NAME_CAPTURE_IDENTIFIER))
-	// REGEX_DID_METHOD_ID = regexp.MustCompile(fmt.Sprintf("(?<%s>%s+(:%s+)*)", REGEX_DID_METHOD_ID_CAPTURE_IDENTIFIER, REGEX_DID_ID_CHAR, REGEX_DID_ID_CHAR))
-	// REGEX_DID_PATH = regexp.MustCompile(fmt.Sprintf("(?<%s>\\/[^#?]*)?", REGEX_DID_PATH_CAPTURE_IDENTIFIER))
-	// REGEX_DID_QUERY = regexp.MustCompile(fmt.Sprintf("(?<%s>[?][^#]*)?", REGEX_DID_QUERY_CAPTURE_IDENTIFIER))
-	// REGEX_DID_FRAGMENT = regexp.MustCompile(fmt.Sprintf("(?<%s>\\#.*)?", REGEX_DID_FRAGMENT_CAPTURE_IDENTIFIER))
-	// REGEX_DID_PARAM = regexp.MustCompile(fmt.Sprintf(";(?<%s>%s+)=(?<%s>%s*)", REGEX_DID_PARAM_NAME_CAPTURE_IDENTIFIER, REGEX_DID_PARAM_CHAR, REGEX_DID_PARAM_VALUE_CAPTURE_IDENTIFIER, REGEX_DID_PARAM_CHAR))
-	// REGEX_DID_PARAMS = regexp.MustCompile(fmt.Sprintf("(?<%s>(%s)*)", REGEX_DID_PARAMS_CAPTURE_IDENTIFIER, REGEX_DID_PARAM))
-	// REGEX_DID_URL = regexp.MustCompile(fmt.Sprintf("^did:%s:%s%s%s%s%s$", REGEX_DID_METHOD_NAME, REGEX_DID_METHOD_ID, REGEX_DID_PARAMS, REGEX_DID_PATH, REGEX_DID_QUERY, REGEX_DID_FRAGMENT))
+	REGEX_DID_METHOD_NAME = regexp.MustCompile(fmt.Sprintf("(?P<%s>[a-zA-Z0-9_]+)", REGEX_DID_METHOD_NAME_CAPTURE_IDENTIFIER))
+	REGEX_DID_METHOD_ID = regexp.MustCompile(fmt.Sprintf("(?P<%s>%s+(:%s+)*)", REGEX_DID_METHOD_ID_CAPTURE_IDENTIFIER, REGEX_DID_ID_CHAR, REGEX_DID_ID_CHAR))
+	REGEX_DID_PATH = regexp.MustCompile(fmt.Sprintf("(?P<%s>\\/[^#?]*)?", REGEX_DID_PATH_CAPTURE_IDENTIFIER))
+	REGEX_DID_QUERY = regexp.MustCompile(fmt.Sprintf("(?P<%s>[?][^#]*)?", REGEX_DID_QUERY_CAPTURE_IDENTIFIER))
+	REGEX_DID_FRAGMENT = regexp.MustCompile(fmt.Sprintf("(?P<%s>\\#.*)?", REGEX_DID_FRAGMENT_CAPTURE_IDENTIFIER))
+	REGEX_DID_PARAM = regexp.MustCompile(fmt.Sprintf(";(?P<%s>%s+)=(?P<%s>%s*)", REGEX_DID_PARAM_NAME_CAPTURE_IDENTIFIER, REGEX_DID_PARAM_CHAR, REGEX_DID_PARAM_VALUE_CAPTURE_IDENTIFIER, REGEX_DID_PARAM_CHAR))
+	REGEX_DID_PARAMS = regexp.MustCompile(fmt.Sprintf("(?P<%s>(%s)*)", REGEX_DID_PARAMS_CAPTURE_IDENTIFIER, REGEX_DID_PARAM))
+	REGEX_DID_URL = regexp.MustCompile(fmt.Sprintf("^did:%s:%s%s%s%s%s$", REGEX_DID_METHOD_NAME, REGEX_DID_METHOD_ID, REGEX_DID_PARAMS, REGEX_DID_PATH, REGEX_DID_QUERY, REGEX_DID_FRAGMENT))
 )
 
 type DidTokenFactory struct {
@@ -53,26 +55,22 @@ func (didf DidTokenFactory) Create(creator string, url string) *didTypes.Did {
 		Creator: creator,
 	}
 
-	//TODO: Resolve regexp issues and finish mapping params
-	//if REGEX_DID_URL.MatchString(url) {	
-		// did.Url = url
-		// did.MethodName = REGEX_DID_METHOD_NAME.FindStringSubmatch(url)[0]
-		// did.MethodId = REGEX_DID_METHOD_ID.FindStringSubmatch(url)[0]
-		// did.Path = REGEX_DID_PATH.FindStringSubmatch(url)[0]
-		// did.Query = REGEX_DID_QUERY.FindStringSubmatch(url)[0]
-		// did.Fragment = REGEX_DID_FRAGMENT.FindStringSubmatch(url)[0]
+	if REGEX_DID_URL.MatchString(url) {	
+		did.Url = url
+		did.MethodName = REGEX_DID_METHOD_NAME.FindStringSubmatch(url)[0]
+		did.MethodId = REGEX_DID_METHOD_ID.FindStringSubmatch(url)[0]
+		did.Path = REGEX_DID_PATH.FindStringSubmatch(url)[0]
+		did.Query = REGEX_DID_QUERY.FindStringSubmatch(url)[0]
+		did.Fragment = REGEX_DID_FRAGMENT.FindStringSubmatch(url)[0]
 
-		// params := REGEX_DID_PARAMS.FindStringSubmatch(url)
+		params := REGEX_DID_PARAMS.FindStringSubmatch(url)
 
-		// if len(params) > 0 {
-		// 	did.Parameters = []*didTypes.DidParameter{}
-
-		// 	for _, param := range REGEX_DID_PARAM.FindStringSubmatch(params[0]) {
-		// 		_ = param
-		// 		did.Parameters = append(did.Parameters, &didTypes.DidParameter{})
-		// 	}
-		// }
-	//}
+		if len(params) > 0 {
+			did.Parameters = []*didTypes.DidParameter{}
+			
+			//TODO: Extract params
+		}
+	}
 	
 	return did
 }
