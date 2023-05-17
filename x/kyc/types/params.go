c5fb4f1c9f5bcb91ee546a4bd450493f947ca20b
ZaradarBH <tobias.andersen79@gmail.com>
2023-05-17 22:25:56 +0200

    Approver params for kyc/aml module

--- x/kyc/types/params.go
+++ x/kyc/types/params.go
@@ -1,11 +1,20 @@
 package types
 
 import (
+	"fmt"
+	didTypes "github.com/be-heroes/doxchain/x/did/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"gopkg.in/yaml.v2"
 )
 
 var _ paramtypes.ParamSet = (*Params)(nil)
+
+// Parameter store key
+var (
+	DefaultApprovers = []didTypes.Did(nil) // none allowed
+
+	ParamStoreKeyApprovers = []byte("Approvers")
+)
 
 // ParamKeyTable the param key table for launch module
 func ParamKeyTable() paramtypes.KeyTable {
@@ -13,22 +22,40 @@ func ParamKeyTable() paramtypes.KeyTable {
 }
 
 // NewParams creates a new Params instance
-func NewParams() Params {
-	return Params{}
+func NewParams(approvers []didTypes.Did) Params {
+	return Params{
+		Approvers: approvers,
+	}
 }
 
 // DefaultParams returns a default set of parameters
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(DefaultApprovers)
 }
 
 // ParamSetPairs get the params.ParamSet
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(ParamStoreKeyApprovers, &p.Approvers, validateApprovers),
+	}
+}
+
+func validateApprovers(i interface{}) error {
+	_, ok := i.([]didTypes.Did)
+
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	return nil
 }
 
 // Validate validates the set of params
 func (p Params) Validate() error {
+	if err := validateApprovers(p.Approvers); err != nil {
+		return err
+	}
+
 	return nil
 }
 
