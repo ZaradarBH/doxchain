2af019a39bd5c543d671d315b91641da7b9800da
BambooDM <bamboo.nebula.2vn@gmail.com>
2023-10-03 23:12:43 +0700

    cover a did document unit test case

--- x/did/keeper/msg_server_test.go
+++ x/did/keeper/msg_server_test.go
@@ -26,6 +26,38 @@ func (s *KeeperTestSuite) TestCreateDIDDocument() {
 	s.Require().NotNil(didDocument)
 }
 
+// go test -v -run ^TestKeeperTestSuite/TestDeleteDIDDocument$ github.com/be-heroes/doxchain/x/did/keeper
+func (s *KeeperTestSuite) TestDeleteDIDDocument() {
+	s.SetupTest()
+
+	creator := s.TestAccs[0]
+
+	// create DID document
+	msgServer := keeper.NewMsgServerImpl(s.App.DidKeeper)
+	msg := types.NewMsgCreateDidDocumentRequest(creator.String(), *s.GetDidDocument(creator))
+	err := msg.ValidateBasic()
+	s.Require().NoError(err)
+	res, err := msgServer.CreateDidDocument(sdk.WrapSDKContext(s.Ctx), msg)
+	s.Require().NoError(err)
+	s.Require().NotNil(res)
+
+	// check if has stored
+	didDocument, found := s.App.DidKeeper.GetDidDocument(s.Ctx, msg.DidDocument.Id.GetW3CIdentifier())
+	s.Require().True(found)
+	s.Require().NotNil(didDocument)
+
+	// delete DID document
+	msgDelete := types.NewMsgDeleteDidDocumentRequest(creator.String(), didDocument.Id.GetW3CIdentifier())
+	err = msgDelete.ValidateBasic()
+	s.Require().NoError(err)
+	resDelete, err := msgServer.DeleteDidDocument(sdk.WrapSDKContext(s.Ctx), msgDelete)
+	s.Require().NoError(err)
+	s.Require().NotNil(resDelete)
+
+	didDocument, found = s.App.DidKeeper.GetDidDocument(s.Ctx, msg.DidDocument.Id.GetW3CIdentifier())
+	s.Require().False(found)
+}
+
 // go test -v -run ^TestKeeperTestSuite/TestCreateDID$ github.com/be-heroes/doxchain/x/did/keeper
 func (s *KeeperTestSuite) TestCreateDID() {
 	s.SetupTest()
